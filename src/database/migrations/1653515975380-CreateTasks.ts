b71962d49b920c5f58e26eb4906511891f25e12c
edumoreira1506 <contato@eduardoem.com.br>
2022-05-25 19:03:34 -0300

    Add tasks table

---
+++ src/database/migrations/1653515975380-CreateTasks.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateTasks1653515975380 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(new Table({
+      name: 'tasks',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()',
+          isNullable: false,
+        },
+        {
+          name: 'content',
+          type: 'varchar',
+          isNullable: false
+        },
+        {
+          name: 'finished',
+          type: 'boolean',
+          isNullable: false,
+          default: false
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'CURRENT_TIMESTAMP(6)'
+        }
+      ]
+    }), true)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks')
+  }
+}
